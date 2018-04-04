ad27d5b931029fd156b949c3cf1b1a466f61a083
Renato Zannon <renato@rrsz.com.br>
2018-04-04 10:52:41 -0300

    WIP

--- src/github_client.rs
+++ src/github_client.rs
@@ -1,11 +1,12 @@
 use std::env;
-use reqwest::header::{Authorization, Headers};
+use reqwest::header::{self, Authorization, Headers};
 use reqwest::unstable::async::Client;
 use failure::Error;
 use tokio_core::reactor::Handle;
 use notification::{Notification, ReviewRequest, PullRequest};
 use futures::prelude::*;
 use futures::{future, stream};
+use futures::future::Either;
 
 pub struct GithubClient {
     http: Client,
@@ -32,6 +33,44 @@ impl GithubClient {
             let requests = notifications.into_iter().filter_map(ReviewRequest::from_notification);
             stream::iter_ok(requests)
         }).flatten_stream().map_err(Error::from)
+    }
+
+    fn get_all_notifications<'a>(&'a self) -> impl Stream<Item = Notification, Error = Error> + 'a {
+        let url = Some("https://api.github.com/notifications".to_owned());
+
+        fn make_request<'b>(http: &'b Client) -> impl Future<Item = (Vec<Notification>, Option<String>), Error = Error> + 'b {
+            use reqwest::header::RelationType;
+
+            http.get(&page_url).send().and_then(|mut response| {
+                let link_header: Option<&header::Link> = response.headers().get();
+                let next_url: Option<String> = link_header.and_then(|header| {
+                    for value in header.values() {
+                        let includes = value.rel().map(|rels| rels.contains(&RelationType::Next));
+
+                        match includes {
+                            Some(true) => return Some(value.link().to_owned()),
+                            _ => continue,
+                        }
+                    }
+
+                    return None;
+                });
+
+                response.json::<Vec<Notification>>().map(|notifications| {
+                    return (notifications, next_url);
+                })
+            }).map_err(Error::from)
+        }
+
+        stream::unfold(url, |maybe_url| {
+            let page_url = match maybe_url {
+                Some(url) => url,
+                None => return None,
+            };
+
+            let response = make_request(&self.http);
+            return Some(response);
+        }).and_then(stream::iter_ok)
     }
 
     pub fn pull_requests_to_review<'a>(&'a self) -> impl Stream<Item = PullRequest, Error = Error> + 'a {
