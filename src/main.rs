31813dec2b48bdf691daa4a9530cb9552f463fbe
Renato Zannon <renato@rrsz.com.br>
2018-04-19 00:10:20 -0300

    Basic slog usage on main

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,11 @@ extern crate tokio_core;
 extern crate tokio_retry;
 extern crate tokio_timer;
 
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
+
 mod github_client;
 mod notification;
 mod notification_stream;
@@ -28,37 +33,46 @@ use futures::prelude::*;
 use futures::future;
 
 fn main() {
+    let logger = configure_slog();
     env_logger::init();
     openssl_probe::init_ssl_cert_env_vars();
 
-    let err = match run() {
+    let err = match run(logger.clone()) {
         Ok(_) => return,
         Err(err) => err,
     };
 
     if let Some(bt) = err.cause().backtrace() {
-        eprintln!("{}", bt);
+        error!(logger, "backtrace" => bt);
     } else {
-        eprintln!("{:?}", err.cause());
+        error!(logger, "cause" => ?err.cause());
     }
 }
 
-fn run() -> Result<(), Error> {
+fn run(logger: slog::Logger) -> Result<(), Error> {
     dotenv().ok();
 
     let mut core = reactor::Core::new()?;
     let client = github_client::GithubClient::new(&core.handle())?;
 
     let future = {
-        future::loop_fn(client, |client| {
+        let mut batch_number = 0;
+        let logger = &logger;
+
+        future::loop_fn(client, move |client| {
+            batch_number += 1;
+
             let get_batch = move || {
                 let (next_client, current_batch) = client.poll_review_requests();
+                let batch_logger = logger.new(o!("batch_number" => batch_number));
 
                 client.wait_poll_interval().and_then(move |_| {
-                    current_batch.inspect_err(|err| {
-                        eprintln!("Error: {}", err);
-                    }).for_each(|pull_request| {
-                        println!("- {:?}", pull_request);
+                    let err_batch_logger = batch_logger.clone();
+
+                    current_batch.inspect_err(move |err| {
+                        error!(err_batch_logger, "Error in PullRequest batch"; "error" => %err);
+                    }).for_each(move |pull_request| {
+                        info!(batch_logger, "PR received"; "pull_request" => ?pull_request);
                         future::ok(())
                     })
                 }).and_then(move |_| next_client)
@@ -78,4 +92,14 @@ fn run() -> Result<(), Error> {
     };
 
     core.run(future)
+}
+
+fn configure_slog() -> slog::Logger {
+    use slog::Drain;
+
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(drain, o!())
 }
