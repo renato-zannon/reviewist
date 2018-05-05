50cefb696ab6c6b13ce0d68e7c0e11358eab3c51
Renato Zannon <renato@rrsz.com.br>
2018-05-05 13:56:27 -0300

    WIP

--- fake_github/src/lib.rs
+++ fake_github/src/lib.rs
@@ -8,10 +8,14 @@ extern crate serde_derive;
 
 use std::env;
 use hyper::StatusCode;
+use std::net::SocketAddr;
+
+use ipc_channel::ipc;
+
 use gotham::http::response::create_response;
 use gotham::state::State;
-use ipc_channel::ipc;
-use std::net::SocketAddr;
+use gotham::router::Router;
+use gotham::router::builder::*;
 
 pub enum Message {
 }
@@ -21,38 +25,44 @@ pub enum Response {
     Booted { port: SocketAddr },
 }
 
-/// Create a `Handler` which is invoked when responding to a `Request`.
-///
-/// How does a function become a `Handler`?.
-/// We've simply implemented the `Handler` trait, for functions that match the signature used here,
-/// within Gotham itself.
 fn say_hello(state: State) -> (State, hyper::Response) {
+    let response_string = String::from(include_str!("../data/notifications.json"));
+
     let res = create_response(
         &state,
         StatusCode::Ok,
-        Some((String::from("{}").into_bytes(), mime::TEXT_PLAIN)),
+        Some((response_string.into_bytes(), mime::APPLICATION_JSON)),
     );
 
     (state, res)
 }
 
-/// Start a server and call the `Handler` we've defined above for each `Request` we receive.
+fn router() -> Router {
+    build_simple_router(|route| {
+        route.get("/notifications").to(say_hello);
+    })
+}
+
 pub fn run() {
-    let mut args: Vec<String> = env::args().skip(1).collect();
-
-    let server_path = args.pop().unwrap();
     let addr = get_open_port();
+    let sender = build_sender();
 
-    let sender = ipc::IpcSender::<Response>::connect(server_path).unwrap();
     sender
         .send(Response::Booted { port: addr.clone() })
         .unwrap();
 
-    gotham::start(addr, || Ok(say_hello))
+    gotham::start(addr, router());
 }
 
 fn get_open_port() -> SocketAddr {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").unwrap();
     let addr = listener.local_addr().unwrap();
     return addr;
+}
+
+fn build_sender() -> ipc::IpcSender<Response> {
+    let mut args: Vec<String> = env::args().skip(1).collect();
+    let server_path = args.pop().unwrap();
+
+    ipc::IpcSender::connect(server_path).unwrap()
 }
