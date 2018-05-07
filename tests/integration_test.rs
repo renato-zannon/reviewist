a1a450a7cd695b9e33e3d870e9456aa9e9286f7f
Renato Zannon <renato@rrsz.com.br>
2018-05-06 23:54:47 -0300

    Extract server setup code

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -19,13 +19,34 @@ use fake_github::{Message, Response};
 
 #[test]
 fn test_smoke() {
+    let result = with_fake_server(|server| {
+        let mut core = Core::new().expect("failed to start tokio core");
+
+        let future = reviewist::run(Config {
+            logger: configure_slog(),
+            core: &core,
+            github_base: Url::parse(&format!("http://{}/github/", server.address)).unwrap(),
+            todoist_base: Url::parse("http://example.com").unwrap(),
+        });
+
+        core.run(future)
+    });
+
+    result.unwrap();
+}
+
+struct FakeServer {
+    receiver: ipc::IpcReceiver<Response>,
+    address: std::net::SocketAddr,
+}
+
+fn with_fake_server<T>(f: impl FnOnce(FakeServer) -> T) -> T {
     let (server, server_name) = ipc::IpcOneShotServer::<Response>::new().unwrap();
 
     let mut fake_github = Command::new("cargo")
         .args(&["run", "-p", "fake_github", "--", server_name.as_ref()])
         .spawn()
         .expect("failed to start fake github");
-    let mut core = Core::new().expect("failed to start tokio core");
 
     let db_path = new_database();
     env::set_var("DATABASE_URL", db_path.fd_path());
@@ -35,19 +56,12 @@ fn test_smoke() {
     let (receiver, message) = server.accept().unwrap();
     let address = match message {
         Response::Booted { port } => port,
-        _ => panic!("WTF?"),
     };
 
-    let future = reviewist::run(Config {
-        logger: configure_slog(),
-        core: &core,
-        github_base: Url::parse(&format!("http://{}/github/", address)).unwrap(),
-        todoist_base: Url::parse("http://example.com").unwrap(),
-    });
-
-    let result = core.run(future);
+    let result = f(FakeServer { receiver, address });
     fake_github.kill().unwrap();
-    result.unwrap();
+
+    result
 }
 
 fn configure_slog() -> slog::Logger {
